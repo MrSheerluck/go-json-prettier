5addce6e6b3faa187ef189a51613e5dbfceb649c
Mr Sheerluck <mrsheerluck003@gmail.com>
2026-01-19 19:11:08 +0530

    added multifile support, added console output and inplace file editing option

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Print("JSON Prettier")
+	writeFile := flag.Bool("w", false, "write formatted JSON back to file")
+	flag.Parse()
+	files := flag.Args()
+	for i := range files {
+		content, err := os.ReadFile(files[i])
+		if err != nil {
+			os.Exit(1)
+		}
+		var data interface{}
+		err = json.Unmarshal(content, &data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s", err)
+		}
+		prettyJSON, err := json.MarshalIndent(data, "", "  ")
+		if err != nil {
+				fmt.Fprintf(os.Stderr, "Error formatting: %v\n", err)
+				continue
+		}
+		if *writeFile {
+			err = os.WriteFile(files[i], prettyJSON, 0644)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error writing file: %v\n", err)
+				}
+		} else {
+				fmt.Printf("%s\n", prettyJSON)
+		}
+	}
 }
